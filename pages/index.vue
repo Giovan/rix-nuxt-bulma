8c9d76276489e60980e69baa2f5619dcf08868a8
Giovan <giovanniyatze@gmail.com>
2020-03-07 16:20:07 -0600

    Add comment for login component

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Rix
-      </h1>
-      <h2 class="subtitle">
-        Rix admin panel
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
   </div>
 </template>
-
 <script>
-import Logo from '~/components/Logo.vue'
+// import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
+    //Logo
   }
 }
 </script>
-
-<style>
+<style scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
