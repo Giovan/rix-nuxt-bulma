0517140411690957f6ac57f1611b9e27412dbbc6
Giovan <giovanniyatze@gmail.com>
2020-03-07 17:21:25 -0600

    Add forms page and definition

---
+++ pages/forms.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <NavBar />
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <div class="left-menu">
+          <Menu />
+        </div>
+      </div>
+      <div class="column">
+        2
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Menu from '~/components/primary/menu/index.vue'
+import NavBar from '~/components/primary/navbar/index.vue'
+import Footer from '~/components/primary/footer/index.vue'
+
+export default {
+  components: {
+    Menu,
+    NavBar,
+    Footer
+  }
+}
+</script>
+<style scoped>
+</style>
