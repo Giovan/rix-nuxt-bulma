3c03a8b6ed3020a6e48deb1e42750a925ba8ad4b
Giovan <giovanniyatze@gmail.com>
2020-03-07 18:43:40 -0600

    Add credit card definition

---
+++ components/forms/credit-card/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card login-card-custom">
+    <div class="card-content">
+      <p class="title">
+        Rix
+      </p>
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" type="email" placeholder="example@gmail.com">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Credit Card Number</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="*********">
+        </div>
+      </div>
+
+      <div class="field is-grouped">
+        <label class="label">Credit Card Number</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="*********">
+        </div>
+        <label class="label">Credit Card Number</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="*********">
+        </div>
+        <label class="label">Credit Card Number</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="*********">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style scoped>
+</style>
