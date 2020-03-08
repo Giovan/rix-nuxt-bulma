ab6ff47ea1fa6fd40f0f95e9b307723093591076
Giovan <giovanniyatze@gmail.com>
2020-03-07 20:39:45 -0600

    Add signup page and form definition

---
+++ components/forms/signup/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <!-- <NavBar /> -->
+      </div>
+      <div class="column">
+        <!-- <SignUpForm /> -->
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import NavBar from '~/components/primary/navbar/index.vue'
+export default {
+  components: {
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ pages/signup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <NavBar />
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <SignUpForm />
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import NavBar from '~/components/primary/navbar/index.vue'
+import Footer from '~/components/primary/footer/index.vue'
+import SignUpForm from '~/components/forms/signup/index.vue'
+
+export default {
+  components: {
+    NavBar,
+    Footer,
+    SignUpForm,
+  }
+}
+</script>
+<style scoped>
+</style>
