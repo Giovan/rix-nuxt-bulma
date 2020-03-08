d7532e137f0a7e60f3868ae0ddc5fa6b2d776b79
Giovan <giovanniyatze@gmail.com>
2020-03-07 18:32:24 -0600

    Add login form definition

--- components/forms/login/index.vue
+++ components/forms/login/index.vue
@@ -1,32 +1,18 @@
 <template>
   <div>
-    <div class="card">
+    <div class="card login-card-custom">
       <div class="card-content">
         <p class="title">
-          “There are two hard things in computer science: cache invalidation, naming things, and off-by-one errors.”
+          Rix
         </p>
         <p class="subtitle">
-          Jeff Atwood
+          Log In
         </p>
         <div class="field">
-          <label class="label">Name</label>
+          <label class="label">Email</label>
           <div class="control">
-            <input class="input" type="text" placeholder="Text input">
+            <input class="input" type="email" placeholder="Text input">
           </div>
-        </div>
-
-        <div class="field">
-          <label class="label">Username</label>
-          <div class="control has-icons-left has-icons-right">
-            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
-            <span class="icon is-small is-left">
-              <i class="fas fa-user"></i>
-            </span>
-            <span class="icon is-small is-right">
-              <i class="fas fa-check"></i>
-            </span>
-          </div>
-          <p class="help is-success">This username is available</p>
         </div>
 
         <div class="field">
@@ -44,42 +30,10 @@
         </div>
 
         <div class="field">
-          <label class="label">Subject</label>
-          <div class="control">
-            <div class="select">
-              <select>
-                <option>Select dropdown</option>
-                <option>With options</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div class="field">
-          <label class="label">Message</label>
-          <div class="control">
-            <textarea class="textarea" placeholder="Textarea"></textarea>
-          </div>
-        </div>
-
-        <div class="field">
           <div class="control">
             <label class="checkbox">
               <input type="checkbox">
               I agree to the <a href="#">terms and conditions</a>
-            </label>
-          </div>
-        </div>
-
-        <div class="field">
-          <div class="control">
-            <label class="radio">
-              <input type="radio" name="question">
-              Yes
-            </label>
-            <label class="radio">
-              <input type="radio" name="question">
-              No
             </label>
           </div>
         </div>
@@ -93,21 +47,15 @@
           </div>
         </div>
       </div>
-      <footer class="card-footer">
-        <p class="card-footer-item">
-          <span>
-            View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
-          </span>
-        </p>
-        <p class="card-footer-item">
-          <span>
-            Share on <a href="#">Facebook</a>
-          </span>
-        </p>
-      </footer>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.login-card-custom {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  max-width: 85%!important;
+}
 </style>
